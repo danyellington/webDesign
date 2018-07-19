c177eadf0e7774d1bc5296df9bbfc6aa1fb16df6
Danyel Murray <danyelmurray1987@gmail.com>
2018-07-18 19:13:37 -0700

    add styles and clickable icons

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+        $("#icon").click(function() {
+          $("#hide").fadeOut();
+          $("#fade").fadeIn();
+          $("#fade2").fadeOut();
+        });
+      });
+
+
+        $(function() {
+         $("#icon2").click(function() {
+          $("#hide").fadeOut();
+          $("#fade").fadeOut();
+          $("#fade2").fadeIn();
+        });
+      });
+
+
+      $(function() {
+        $("#icon4").click(function() {
+         $("#hide").fadeOut();
+         $("#fade").fadeOut();
+         $("#fade3").fadeToggle();
+       });
+     });
